1b79e72a8969cd20abb48acc191e8058ce3dde10
Tim Leytens <tim.leytens@icapps.com>
2018-12-07 11:09:21 +0100

    Initial Commit

---
+++ testpath/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testpath
+//
+//  Created by Tim Leytens on 07/12/2018.
+//  Copyright © 2018 Tim Leytens. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
